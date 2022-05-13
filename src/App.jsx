a8a045fdf10216faee23cff16b9235a48140c8f3
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2022-05-13 18:00:10 -0500

    Adding react-router library and completing the header component with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Container from 'react-bootstrap/Container'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
 
 import './App.scss'
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
-      <main className="content">
-        Content
-      </main>
-      <Footer />
+      <Router>
+        <Header />
+        <main className="content">
+          <Container>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom"
+import LinkContainer from "react-router-bootstrap/LinkContainer"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Form from "react-bootstrap/Form"
+import Nav from "react-bootstrap/Nav"
+
+import './styles/Header.scss'
+
 const Header = () => {
     return (
         <header className="header">
-            Header
+            <Container>
+                <Row>
+                    <Col className="left">
+                        <Link to="/">
+                            <h1>Blog JS</h1>
+                        </Link>
+                    </Col>
+                    <Col className="center" xs={6}>
+                        <Form>
+                            <Form.Control type="search" />
+                        </Form>
+                    </Col>
+                    <Col className="right">
+                        <Nav>
+                            <Nav.Item>
+                                <LinkContainer to="/login">
+                                    <Nav.Link>Iniciar sesión</Nav.Link>
+                                </LinkContainer>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <LinkContainer to="/register">
+                                    <Nav.Link>Registrarse</Nav.Link>
+                                </LinkContainer>
+                            </Nav.Item>    
+                        </Nav>
+                    </Col>
+                </Row>
+            </Container>
         </header>
     )
 }
